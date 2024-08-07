5195801df531ea7ed6076a459fe46bc6c5872b97
msadeedashraf <157824309+msadeedashraf@users.noreply.github.com>
2024-08-07 13:24:49 -0400

    .

--- day12/problem3.py
+++ day12/problem3.py
@@ -1,25 +1,38 @@
+from random import randint
+
+
 class AirTravel:
+    to = None
+    fro = None
+
     def __init__(self, flightNo, airLine, departTime):
         self.flightNo = flightNo
         self.airLine = airLine
         self.departTime = departTime
 
     def booking(self, to, fro):
-        print(f"Your ticked is booked in {self.flightNo} traveling from {fro} to {to}")
+        self.to = to
+        self.fro = fro
+        print(
+            f"Your ticked is booked in {self.flightNo} traveling from {self.fro} to {self.to}"
+        )
 
     def checkStatus(self):
         print(
             f"Flight no {self.flightNo} with {self.airLine} is departing at {self.departTime}"
         )
 
-    def checkFare(self, to, fro):
-        print(f"You fare for traveling from from {fro} to {to} is $1000 ")
+    # def checkFare(self, to, fro):
+    def checkFare(self):
+        print(
+            f"You fare for traveling from from {self.fro} to {self.to} is {randint(250,5000)} "
+        )
 
 
 myTravelPlan = AirTravel("AC201", "Delta", "1340")
 
 myTravelPlan.booking("London", "Toronto")
 myTravelPlan.checkStatus()
-myTravelPlan.checkFare("London", "Toronto")
-# myTravelPlan.checkFare()
+# myTravelPlan.checkFare("London", "Toronto")
+myTravelPlan.checkFare()
 # Change the above code where checkFare is not asking for to and from parameter
