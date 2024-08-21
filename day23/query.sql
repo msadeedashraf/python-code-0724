496fc14893e8ada6c8394ff76eb159ad13fe9f71
msadeedashraf <157824309+msadeedashraf@users.noreply.github.com>
2024-08-21 13:05:43 -0400

    .

--- day23/query.sql
+++ day23/query.sql
@@ -34,3 +34,27 @@ inner join SalesLT.Address as a
 on ca.AddressID = a.AddressID
 and a.CountryRegion= 'Canada'
 
+ --select all column and rows from a table
+ SELECT * FROM SalesLT.Product;
+ 
+ 
+ --select all column and rows from a table and assigning a alias
+ SELECT * FROM SalesLT.Product as p;
+ 
+ --selecting column from the table
+ SELECT p.Name,p.StandardCost, p.ListPrice FROM SalesLT.Product as p;
+
+
+ SELECT Name,ListPrice ,StandardCost, ListPrice - StandardCost
+ FROM SalesLT.Product;
+
+ SELECT Name AS ProductName, ListPrice - StandardCost AS Markup
+ FROM SalesLT.Product;
+
+ SELECT ProductNumber, Color, Size, Color + ', ' + Size AS ProductDetails
+FROM SalesLT.Product;
+
+ SELECT ProductNumber, cOLoR, Size, Color + ', ' + Size AS ProductDetails
+FROM SalesLT.Product;
+
+select c.FirstName+' ' + c.MiddleName+' ' +c.LastName as 'Full Name' from SalesLT.Customer as c
